2913743eaae5d22be935a593890a541b465262b0
Aitor Reviriego Amor <aitorevi@gmail.com>
2023-03-24 14:07:45 +0100

    green: return_empty_list_string_is_char

--- src/test/java/MarkdownTransformationShould.java
+++ src/test/java/MarkdownTransformationShould.java
@@ -1,9 +1,9 @@
 /*
-* "" -> []
-* "a" -> []
-* "any text [a link](url)" -> ["[a link](url)"]
-* "any text [a link](url) more text [another link](url)" -> ["[a link](url)", "[another link](url)"]
-*/
+ * "" -> [] *
+ * "a" -> []
+ * "any text [a link](url)" -> ["[a link](url)"]
+ * "any text [a link](url) more text [another link](url)" -> ["[a link](url)", "[another link](url)"]
+ */
 
 import jdk.jshell.spi.ExecutionControl;
 import org.junit.jupiter.api.Assertions;
@@ -11,18 +11,24 @@ import org.junit.jupiter.api.Test;
 
 public class MarkdownTransformationShould {
     @Test
-    void return_empty_list_string_is_empty(){
+    void return_empty_list_string_is_empty() {
         String empty = "";
 
         String[] result = LinkSearcher.find(empty);
 
         Assertions.assertArrayEquals(new String[0], result);
+    }
+    @Test
+    void return_empty_list_string_is_char() {
+        String text = "a";
 
+        String[] result = LinkSearcher.find(text);
+
+        Assertions.assertArrayEquals(new String[0], result);
     }
 
     private static class LinkSearcher {
-        public static String[] find(String empty) {
-            
+        public static String[] find(String text) {
             return new String[0];
         }
     }
